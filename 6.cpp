e9f454ba494dcf64073f660909538ecf26c259bd
ManvendrasinhJadeja <jadejamanvendrasinh79@gmail.com>
2024-12-29 12:37:10 +0530

    SOLVED Question6

--- 6.cpp
+++ 6.cpp
@@ -4,10 +4,82 @@
 // Input: [38, 27, 43, 3, 9]
 // Output Example:
 // Output: [3, 9, 27, 38, 43]
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    
+void merge(int start, int mid, int end, int arr[])
+{
+
+    vector<int> temp(end - start + 1);
+    int left = start, right = mid + 1, index = 0;
+
+    while (left <= mid && right <= end)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp[index] = arr[left];
+            left++, index++;
+        }
+        else
+        {
+            temp[index] = arr[right];
+            right++, index++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp[index] = arr[left];
+        left++, index++;
+    }
+    while (right <= end)
+    {
+        temp[index] = arr[right];
+        right++, index++;
+    }
+
+    index = 0;
+    while (start <= end)
+    {
+        arr[start] = temp[index];
+        start++, index++;
+    }
+}
+void mergeSort(int start, int end, int arr[])
+{
+
+    if (start >= end)
+    {
+        return;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    mergeSort(start, mid, arr);
+    mergeSort(mid + 1, end, arr);
+
+    merge(start, mid, end, arr);
+}
+
+int main()
+{
+
+    int size;
+    cout << "enter the size of array" << endl;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "enter the arr[" << i << "]";
+        cin >> arr[i];
+    }
+
+    mergeSort(0, size-1, arr);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i]<<" ";
+    }
 }
