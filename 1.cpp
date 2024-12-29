874dbd2c44eb6a5a25add40f635342ec17247916
ManvendrasinhJadeja <jadejamanvendrasinh79@gmail.com>
2024-12-29 12:36:49 +0530

    SOLVED Question1

--- 1.cpp
+++ 1.cpp
@@ -5,10 +5,47 @@
 // Output Example:
 // Output: [5, 4, 3, 2, 1]
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+// Declring the revese function which reverses the array
+void reverse(int n, int arr[])
+{
+    // creating new array
+    int temp[n];
 
+    // reversing the array and saving it into the new temp array
+    for (int i = 0; i < n; i++)
+    {
+        temp[n - i - 1] = arr[i];
+    }
+    // adding the elements from the temp array to  original array
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+    // printing the original array after reversing
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+};
+
+int main()
+{
+    // Create array and take size from user
+    int n;
+    cout << "enter the size of the array : ";
+    cin >> n;
+    int arr[n];
+
+    // take elements from user for the each index of the array
+    for (int i = 0; i < n; i++)
+    {
+        cout << "enter the element  arr[" << i << "]";
+        cin >> arr[i];
+    }
+    // calling the function reverse and passing the arguments
+    reverse(n, arr);
 }
