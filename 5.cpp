338604505b1dbb6191a5d6f5d59eff168040aadb
ManvendrasinhJadeja <jadejamanvendrasinh79@gmail.com>
2024-12-29 15:21:14 +0530

    SOLVED Question5

--- 5.cpp
+++ 5.cpp
@@ -5,10 +5,39 @@
 // Length: 5, Breadth: 10
 // Output Example:
 // Area: 50
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+class Rectangle {
+private:
+    int length, breadth;
+
+public:
+
+    Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+    int Area() {
+        return length * breadth;
+    }
+};
+
+int main() {
+    int length, breadth;
+
+
+    cout << "Enter length of the rectangle: ";
+    cin >> length;
+    cout << "Enter breadth of the rectangle: ";
+    cin >> breadth;
+
+
+    Rectangle a(length, breadth);
+
+
+    int area = a.Area();
+    cout << "Area of the rectangle: " << area << endl;
+
+
 }
