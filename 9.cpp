6797ae60f3ccd17da0ddb5ba6b92927b069e133b
ManvendrasinhJadeja <jadejamanvendrasinh79@gmail.com>
2024-12-29 15:46:28 +0530

    SOLVED Question9

--- 9.cpp
+++ 9.cpp
@@ -12,10 +12,117 @@
 // Matrix Addition Result:
 // 6 8
 // 10 12
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+class Matrix
+{
+public:
+    int mat[2][2];
+
+    // takes the input of the matrix
+    void input()
+    {
+        cout << "Enter 4 elements of the matrix (row-wise): " << endl;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                cin >> mat[i][j];
+            }
+        }
+    }
+
+    // display the matrix
+    void display()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    // add two matrix
+    Matrix add(Matrix other)
+    {
+        Matrix result;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                result.mat[i][j] = mat[i][j] + other.mat[i][j];
+            }
+        }
+        return result;
+    }
+
+    // subtract two matrix
+    Matrix subtract(Matrix other)
+    {
+        Matrix result;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                result.mat[i][j] = mat[i][j] - other.mat[i][j];
+            }
+        }
+        return result;
+    }
+
+    // multiply two matrix
+    Matrix multiply(Matrix other)
+    {
+        Matrix result;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                result.mat[i][j] = 0;
+                for (int k = 0; k < 2; k++)
+                {
+                    result.mat[i][j] += mat[i][k] * other.mat[k][j];
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Matrix A, B;
+
+    cout << "Input Matrix A:" << endl;
+    A.input();
+
+    cout << "Input Matrix B:" << endl;
+    B.input();
+
+    cout << "Matrix A:" << endl;
+    A.display();
+
+    cout << "Matrix B:" << endl;
+    B.display();
+
+    // addition
+    Matrix resultAdd = A.add(B);
+    cout << "Matrix Addition Result:" << endl;
+    resultAdd.display();
+
+    // subtraction
+    Matrix resultSubtract = A.subtract(B);
+    cout << "Matrix Subtraction Result:" << endl;
+    resultSubtract.display();
+
+    // multiplication
+    Matrix resultMultiply = A.multiply(B);
+    cout << "Matrix Multiplication Result:" << endl;
+    resultMultiply.display();
+
+    return 0;
 }
